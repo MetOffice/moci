2ab6636f0c670715117c9b1ae361272daeae0cc2
ericaneininger <enquiries@metoffice.gov.uk>
2020-03-31 09:44:17 +0000

    ticket:388 - For IrinaLP - Added absolute norms check script
    git-svn-id: https://metomi/svn/moci.xm/main/trunk@3502 5d77ff06-061d-4adc-8321-d65833a6d205

---
+++ Utilities/verification_scripts/compare_absnorms.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python
+'''
+*****************************COPYRIGHT******************************
+ (C) Crown copyright 2020 Met Office. All rights reserved.
+
+ Use, duplication or disclosure of this code is subject to the restrictions
+ as set forth in the licence. If no licence has been raised with this copy
+ of the code, the use, duplication or disclosure of it is strictly
+ prohibited. Permission to do so must first be obtained in writing from the
+ Met Office Information Asset Owner at the following address:
+
+ Met Office, FitzRoy Road, Exeter, Devon, EX1 3PB, United Kingdom
+*****************************COPYRIGHT******************************
+
+NAME
+    compare_absnorms.py
+
+DESCRIPTION
+   Compare absolute norms from UM output
+'''
+import sys
+import os
+import re
+
+class Timestep(object):
+    '''
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Class to hold timestep attributes
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    '''
+    def __init__(self, timestamp):
+        self.timestamp = timestamp
+        self.final_norm = None
+        self.initial_norm = None
+        self.iterations = None
+
+    def add_norm(self, iterations, error):
+        '''
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        Add either initial or, if initial exists,  final absolute norm
+        with number of iterations
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        '''
+        if self.initial_norm:
+            self.final_norm = float(error)
+        else:
+            self.initial_norm = float(error)
+        self.iterations = int(iterations)
+
+    def get_value(self, attribute):
+        '''
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        Return the value of the requested attribute
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        '''
+        return getattr(self, attribute)
+
+
+def generate_timestep(filename):
+    '''
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Generator function for extracting norms for each timestep
+    Arguments:
+      filename <type str> File name including full path
+    Yield:
+      <type Timestep>
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    '''
+    if not os.path.isfile(filename):
+        raise SystemExit('[FAIL] File does not exist: {}'.format(filename))
+
+    timestamp_regex = re.compile(
+        r'Atm_Step: Timestep.*(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})'
+        )
+    helmholtz_regex = re.compile(
+        r'\*.*Linear solve for Helmholtz problem.*\*'
+        )
+    norms_regex = re.compile(r'\*\s+\d+\s+\d+\s+(\d+)\s+(\S+)\s+\*')
+
+    with open(filename, 'r') as fhandle:
+        nextstep = None
+        num_calls = 0
+        for line in fhandle.readlines():
+            atmstep = timestamp_regex.search(line)
+            if atmstep:
+                nextstep = Timestep(atmstep.group(1))
+            elif helmholtz_regex.search(line):
+                num_calls += 1
+            elif num_calls > 1:
+                try:
+                    iterations, error = norms_regex.search(line).groups()
+                    nextstep.add_norm(iterations, error)
+                except AttributeError:
+                    pass
+            try:
+                if nextstep.final_norm is not None:
+                    yield nextstep
+                    nextstep = None
+                    num_calls = 0
+            except AttributeError:
+                pass
+
+
+def create_timesteps(file_path):
+    '''
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Input:
+        file_path <type str> -
+            Full path to UM output file fort6.pe000 or fort6.stdout
+    Return:
+        <type dict {model data time: <type Timestep>}> }> -
+            Dictionary of timesteps with associated norms
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    '''
+    entries = {}
+    for timestep in generate_timestep(file_path):
+        entries[timestep.timestamp] = timestep
+
+    return entries
+
+
+def compare_timesteps(tsteps1, tsteps2):
+    '''
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Input:
+        tsteps1 <type dict: {timestamp: Timestep()}> -
+                contains KGO output norms
+        tsteps2 <type dict: {timestamp: Timestep()}> -
+                contains norms to be compared
+
+    Returns:
+        msg <type str> - resulting message
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    '''
+    count_compare = 0
+    return_code = -1
+    diffs = []
+
+    if tsteps1 == {} or tsteps2 == {}:
+        msg = '[ERROR] Norms data not found\n'
+    else:
+        for timestamp in tsteps2:
+            if timestamp in tsteps1:
+                count_compare += 1
+                ts1 = tsteps1[timestamp]
+                ts2 = tsteps2[timestamp]
+                for attribute in ['initial_norm', 'final_norm', 'iterations']:
+                    if ts1.get_value(attribute) != ts2.get_value(attribute):
+                        diffs.append(timestamp)
+                        break
+
+        if diffs:
+            msg = '[ERROR] {} differences in norms at model data times:\n{}\n'.\
+                  format(len(diffs), '\n\t'.join(diffs))
+        else:
+            if count_compare == len(tsteps2):
+                msg = '[ OK ] All {} timesteps checked and norms are OK\n'.\
+                    format(count_compare)
+                return_code = 0
+            elif count_compare == 0:
+                msg = '[ERROR] No matching timesteps between KGO and ' + \
+                      'comparison file\n'
+            elif count_compare < len(tsteps2):
+                msg = '[ERROR] {} timestep norms not found in KGO file\n'.\
+                    format(len(tsteps2) - count_compare)
+
+    return return_code, msg
+
+
+def main():
+    ''' Main function for comp_absnorms.py '''
+    if len(sys.argv) < 3:
+        msg = 'Missing arguments.  Usage: \n\t' + \
+              'python comp_absnorms.py <KGOFILE> <FILE2>\n'
+        sys.stderr.write(msg)
+        sys.exit(2)
+
+    # Initialisation
+    # File A - KGO, File B - pe_output with norms
+    kgo_file = sys.argv[1]
+    compare_file = sys.argv[2]
+
+    return_code, output = compare_timesteps(create_timesteps(kgo_file),
+                                            create_timesteps(compare_file))
+    sys.stdout.write(output)
+    sys.exit(return_code)
+
+
+if __name__ == '__main__':
+    main()
---
+++ Utilities/verification_scripts/unittests/test_compare_norms.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+'''
+*****************************COPYRIGHT******************************
+ (C) Crown copyright 2020 Met Office. All rights reserved.
+
+ Use, duplication or disclosure of this code is subject to the restrictions
+ as set forth in the licence. If no licence has been raised with this copy
+ of the code, the use, duplication or disclosure of it is strictly
+ prohibited. Permission to do so must first be obtained in writing from the
+ Met Office Information Asset Owner at the following address:
+
+ Met Office, FitzRoy Road, Exeter, Devon, EX1 3PB, United Kingdom
+*****************************COPYRIGHT******************************
+
+NAME
+    test_compare_norms.py
+
+DESCRIPTION
+    Unit tests for compare_absnorms.py
+'''
+import os
+import sys
+from copy import deepcopy
+import unittest
+
+HERE = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(HERE, os.pardir))
+import compare_absnorms
+
+
+class TimestepTests(unittest.TestCase):
+    ''' Unit tests for Timestep object '''
+    def setUp(self):
+        self.tstep = compare_absnorms.Timestep('ABCD')
+
+    def tearDown(self):
+        pass
+
+    def test_instantiation(self):
+        ''' Assert instantiation of Timestep object '''
+        self.assertIsInstance(self.tstep, compare_absnorms.Timestep)
+        self.assertEqual(self.tstep.timestamp, 'ABCD')
+        self.assertIsNone(self.tstep.initial_norm)
+        self.assertIsNone(self.tstep.final_norm)
+        self.assertIsNone(self.tstep.iterations)
+
+    def test_add_initial_norm(self):
+        ''' Assert successful addition of an initial norm '''
+        self.tstep.add_norm('4', '1.234e05')
+        self.assertEqual(self.tstep.initial_norm, 1.234e5)
+        self.assertEqual(self.tstep.iterations, 4)
+        self.assertIsNone(self.tstep.final_norm)
+
+    def test_add_final_norm(self):
+        ''' Assert succesful addition of a final norm '''
+        self.tstep.add_norm('4', '1.234e05')
+        self.tstep.add_norm('3', '5.432e01')
+        self.assertEqual(self.tstep.initial_norm, 1.234e5)
+        self.assertEqual(self.tstep.final_norm, 54.32)
+        self.assertEqual(self.tstep.iterations, 3)
+
+    def test_get_timestamp(self):
+        ''' Assert retrieval of the timestamp attribute value '''
+        self.assertEqual(self.tstep.get_value('timestamp'), 'ABCD')
+
+    def test_get_bad_attribute(self):
+        ''' Assert failure to get value of non-existent attribute '''
+        with self.assertRaises(AttributeError):
+            self.tstep.get_value('MyAttribute')
+
+
+class TestCompareFiles(unittest.TestCase):
+    ''' Unit tests for Timestep object '''
+    def setUp(self):
+        self.kgofile = os.path.join(HERE, 'norms_kgo')
+        self.emptyfile = os.path.join(HERE, 'norms_emptyfile')
+        self.kgo = {}
+        for i in range(10):
+            name = 'TS' +str(i)
+            self.kgo[name] = compare_absnorms.Timestep(name)
+            self.kgo[name].add_norm(i, 10^i)
+            self.kgo[name].add_norm(i+1, 10^i-1)
+
+    def tearDown(self):
+        pass
+
+    def test_generate_timestep(self):
+        ''' Assert yield variable type from the timestep generator '''
+        tsteps = list(compare_absnorms.generate_timestep(self.kgofile))
+        self.assertIsInstance(tsteps[0], compare_absnorms.Timestep)
+
+    def test_generate_no_norms(self):
+        ''' Assert yield from a file containing no norms data '''
+        tsteps = list(compare_absnorms.generate_timestep(self.emptyfile))
+        self.assertListEqual(tsteps, [])
+
+    def test_generate_no_file(self):
+        ''' Assert SystemExit from the generator due to non-existent file '''
+        with self.assertRaises(SystemExit):
+            _ = list(compare_absnorms.generate_timestep('NotAFile'))
+
+    def test_create_timesteps(self):
+        ''' Assert creation of a timesteps dictionary '''
+        tsteps = compare_absnorms.create_timesteps(self.kgofile)
+        self.assertIsInstance(tsteps, dict)
+        self.assertIsInstance(tsteps[tsteps.keys()[0]],
+                              compare_absnorms.Timestep)
+
+    def test_compare_timesteps(self):
+        ''' Assert successful comparison of norms '''
+        rcode, msg = compare_absnorms.compare_timesteps(self.kgo, self.kgo)
+        self.assertEqual(0, rcode)
+        self.assertIn('10 timesteps checked and norms are OK', msg)
+
+    def test_compare_different_ts(self):
+        ''' Assert no matching timesteps available in kgo and comparator '''
+        testpe = {}
+        for tstep in self.kgo:
+            testpe[tstep + 'B'] = self.kgo[tstep]
+
+        rcode, msg = compare_absnorms.compare_timesteps(self.kgo, testpe)
+        self.assertEqual(-1, rcode)
+        self.assertIn('No matching timesteps', msg)
+
+    def test_compare_ts_not_in_kgo(self):
+        ''' Assert additional timesteps in the comparison file '''
+        testpe = {}
+        for tstep in self.kgo:
+            if int(tstep[-1]) % 2 == 0:
+                testpe[tstep] = self.kgo[tstep]
+
+        rcode, msg = compare_absnorms.compare_timesteps(testpe, self.kgo)
+        self.assertEqual(-1, rcode)
+        self.assertIn('5 timestep norms not found in KGO file', msg)
+
+    def test_compare_differences(self):
+        ''' Assert differences in norms '''
+        testpe = deepcopy(self.kgo)
+        testpe['TS3'].initial_norm = 5000
+        testpe['TS5'].final_norm = 10
+        testpe['TS7'].iterations = 0
+
+        rcode, msg = compare_absnorms.compare_timesteps(self.kgo, testpe)
+        self.assertEqual(-1, rcode)
+        self.assertIn('3 differences', msg)
+        for tstep in ['TS3', 'TS5', 'TS7']:
+            self.assertIn(tstep, msg)
+
+    def test_compare_empty(self):
+        ''' Assert an empty file as kgo or comparator '''
+        rcode, msg = compare_absnorms.compare_timesteps(self.kgo, {})
+        self.assertEqual(-1, rcode)
+        self.assertIn('Norms data not found', msg)
+
+
+if __name__ == '__main__':
+    unittest.main()
