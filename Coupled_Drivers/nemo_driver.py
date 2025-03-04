3f580f902587d1783ed5af9a17608c13b4e2f09a
pierresiddall <enquiries@metoffice.gov.uk>
2025-03-04 10:18:51 +0000

    **Further Commit** ticket:667 : Updated copyright notice
    git-svn-id: https://metomi/svn/moci.xm/main/trunk@5567 5d77ff06-061d-4adc-8321-d65833a6d205

--- Coupled_Drivers/nemo_driver.py
+++ Coupled_Drivers/nemo_driver.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 '''
 *****************************COPYRIGHT******************************
- (C) Crown copyright 2025 Met Office. All rights reserved.
+ (C) Crown copyright 2023-2025 Met Office. All rights reserved.
 
  Use, duplication or disclosure of this code is subject to the restrictions
  as set forth in the licence. If no licence has been raised with this copy
