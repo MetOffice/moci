1b4368bb61d0a0b3ecafcbc653593910233af141
Pierre Siddall <43399998+Pierre-siddall@users.noreply.github.com>
2026-01-19 16:39:56 +0000

    add shellout.py skeleton

---
+++ Utilities/lib/shellout.py
@@ -0,0 +1,12 @@
+import timer
+import subprocess
+
+
+def exec_subprocess(cmd, verbose=True):
+    """
+    Execute a given shell command
+
+    :param cmd: Description
+    :param verbose: Description
+    """
+    pass
