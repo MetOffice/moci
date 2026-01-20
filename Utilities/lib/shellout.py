de73ecd073fb4472fd44209e7987533de6979785
Pierre Siddall <43399998+Pierre-siddall@users.noreply.github.com>
2026-01-20 10:12:38 +0000

    Execute a subprocess

--- Utilities/lib/shellout.py
+++ Utilities/lib/shellout.py
@@ -1,12 +1,22 @@
 import timer
 import subprocess
+import shlex
+import os
 
 
-def exec_subprocess(cmd, verbose=True):
+def _exec_subprocess(cmd, verbose=True,current_working_directory=os.getcwd()):
     """
     Execute a given shell command
 
-    :param cmd: Description
-    :param verbose: Description
+    :param cmd: The command to be executed given as a string
+    :param verbose: A boolean value to determine if the stout
+    stream is displayed during the runtime.
+    :param current_working_directory: The directory in which the
+    command should be executed.
     """
-    pass
+    process = subprocess.run(cmd,
+                             stdin= subprocess.PIPE,
+                             capture_output=True,
+                             cwd=current_working_directory,
+                             timeout=10
+                             )
