7da2a02f1523c9a00bb5c1a8a3575a1858e0f178
Pierre Siddall <43399998+Pierre-siddall@users.noreply.github.com>
2026-01-20 11:49:28 +0000

    Add exception handling

--- Utilities/lib/shellout.py
+++ Utilities/lib/shellout.py
@@ -15,18 +15,29 @@ def _exec_subprocess(cmd, verbose=True, current_working_directory=os.getcwd()):
     :param current_working_directory: The directory in which the
     command should be executed.
     """
-    output = subprocess.run(
-        cmd,
-        stdin=subprocess.PIPE,
-        capture_output=True,
-        cwd=current_working_directory,
-        timeout=10,
-    )
-    if verbose and output:
-        sys.stdout.write(f"[DEBUG]{output.stdout}\n")
-    if output.stderr and output.returncode != 0:
-        sys.stderr.write(f"[ERROR] {output.stderr}\n")
-    if sys.version_info[0] >= 3:
-        output.stdout = output.stdout.decode()
+    try:
 
-    return output
+        output = subprocess.run(
+            cmd,
+            stdin=subprocess.PIPE,
+            capture_output=True,
+            cwd=current_working_directory,
+            timeout=10,
+        )
+        rcode = output.returncode
+
+        if verbose and output:
+            sys.stdout.write(f"[DEBUG]{output.stdout}\n")
+        if output.stderr and output.returncode != 0:
+            sys.stderr.write(f"[ERROR] {output.stderr}\n")
+        if sys.version_info[0] >= 3:
+            output.stdout = output.stdout.decode()
+
+    except subprocess.CalledProcessError as exc:
+        output = exc.output
+        rcode = exc.returncode
+    except OSError as exc:
+        output = exc.strerror
+        rcode = exc.errno
+
+    return output, rcode
