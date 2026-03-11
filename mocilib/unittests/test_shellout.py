1d6ca423f846aeb2caa81e9d11c0346712d4f72b
Pierre Siddall <43399998+Pierre-siddall@users.noreply.github.com>
2026-03-11 15:10:37 +0000

    fix shellout unittest paths

--- mocilib/unittests/test_shellout.py
+++ mocilib/unittests/test_shellout.py
@@ -4,28 +4,36 @@
 # under which the code may be used.
 # -----------------------------------------------------------------------------
 
+import sys
+import os
 import unittest
 
+sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir))
+import shellout
 
 class ExecTets(unittest.TestCase):
-    """Unit tests for executing shellout commands"""
+    ''' Unit tests for executing shellout commands'''
 
     def test_semicolon_commands(self):
         cmd = "echo Hello There;echo General Kenobi"
-        _, rcode = shellout._exec_subprocess(cmd=cmd)
+        _,rcode = shellout.exec_subprocess(cmd=cmd)
         assert rcode == 0
 
     def test_and_commands(self):
-        cmd = "echo Hello There&&echo General Kenobi"
-        _, rcode = shellout._exec_subprocess(cmd=cmd)
+        cmd ="echo Hello There&&echo General Kenobi"
+        _,rcode = shellout.exec_subprocess(cmd=cmd)
         assert rcode == 0
 
-    def test_called_process_error(self, directory):
+    def test_called_process_error(self,directory):
         cmd = f"ls /{directory}"
-        _, rcode = shellout._exec_subprocess(cmd=cmd)
+        _,rcode = shellout.exec_subprocess(cmd=cmd)
         assert rcode != 0
 
     def test_timeout_expired(self):
         cmd = "sleep 15"
-        _, rcode = shellout._exec_subprocess(cmd=cmd, timeout=10)
+        _,rcode = shellout.exec_subprocess(cmd=cmd,timeout=1)
         assert rcode != 0
+
+
+if __name__ == "__main__":
+    unittest.main()
