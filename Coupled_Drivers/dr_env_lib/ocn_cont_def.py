bde824beada8ea589ca237992c34aebd5e3f2f6f
Pierre Siddall <43399998+Pierre-siddall@users.noreply.github.com>
2026-05-27 14:51:18 +0100

    Update ocn_cont_def environment variables

--- Coupled_Drivers/dr_env_lib/ocn_cont_def.py
+++ Coupled_Drivers/dr_env_lib/ocn_cont_def.py
@@ -18,11 +18,11 @@ DESCRIPTION
     Definition of the environment variables required for the ocean
     controllers
 '''
-
+SI3_ENVIRONMENT_VARS_COMMON = {'SI3_NL': {'default_val': 'namelist_ice_cfg'}}
 SI3_ENVIRONMENT_VARS_INITIAL = {
     'SI3_START': {'default_val': ''},
     'SI3_NL': {'default_val': 'namelist_ice_cfg'}}
-
+SI3_ENVIRONMENT_VARS_FINAL = SI3_ENVIRONMENT_VARS_COMMON
 TOP_ENVIRONMENT_VARS_COMMON = {
     'TOP_NL': {'default_val': 'namelist_top_cfg'}}
 TOP_ENVIRONMENT_VARS_INITIAL = {
@@ -32,3 +32,4 @@ TOP_ENVIRONMENT_VARS_INITIAL = {
 
 TOP_ENVIRONMENT_VARS_INITIAL = {**TOP_ENVIRONMENT_VARS_COMMON,
                                 **TOP_ENVIRONMENT_VARS_INITIAL}
+TOP_ENVIRONMENT_VARS_FINAL = TOP_ENVIRONMENT_VARS_COMMON
